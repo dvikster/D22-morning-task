320d76fc2544a6b01e99c78e4e008fe60e3120c3
dvikster <dvikster@gmail.com>
2017-05-23 11:24:04 +0300

    added files

--- js/js.js
+++ js/js.js
@@ -2,20 +2,43 @@
 $(document).ready(function(){
     $('.button-collapse').sideNav();
     $('.slider').slider();
-    $('table').on('click', changeTableClass);
+    $('#table1').on('click', changeTableClass);
+
+
+    $( "input[name=group1]" ).on("click", function() {
+        $('#table2').removeClass().addClass($( "input:checked" ).val());
+    });
+
+    // через checked
+    // $( "input[type=checkbox]" ).on("change", function() {
+    //     if(this.checked){
+    //         $('#table3').addClass($(this).val());
+    //     }
+    //     else{
+    //         $('#table3').removeClass($(this).val());
+    //     }
+    // });
+
+
+    // через toggleClass
+    $( "input[type=checkbox]" ).on("click", function() {
+        $('#table3').toggleClass($(this).val());
+    });
+
 });
 
 var i = 0;
 function changeTableClass(){
     i++;
     if( i==1 ){
-    $('table').removeClass('highlight').addClass('bordered');
+    $('#table1').removeClass('highlight').addClass('bordered');
     }
     else if( i==2 ){
-        $('table').removeClass('bordered').addClass('striped');
+        $('#table1').removeClass('bordered').addClass('striped');
     }
     else if( i==3 ){
-        $('table').removeClass('striped').addClass('highlight');
+        $('#table1').removeClass('striped').addClass('highlight');
         i=0;
     }
 };
+
